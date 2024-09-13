6a0614dd6dc62a90c9e50ed5ca1a7b76226d18f4
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-09-13 04:40:36 +0200

    Auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,13 +9,18 @@ exports.register = async (req, res) => {
         const existingUser = await User.findOne({ username });
         if (existingUser) {
             return res.status(400).json({ msg: 'Username is already taken' });
+        }else{
+            existingUser = await User.findOne({ email });
+            if (existingUser) {
+                return res.status(400).json({ msg: 'Email is already taken' });
+            }
         }
 
         const user = new User({ username, password, email });
         await user.save();
-        res.status(201).json({ msg: 'User registered successfully' });
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        res.status(400).json({ msg: 'Error registering user', error: error.message });
+        res.status(400).json({ message: 'Error registering user', error: error.message });
     }
 };
 
@@ -24,14 +29,14 @@ exports.login = async (req, res) => {
 
     try {
         const user = await User.findOne({ username });
-        if (!user) return res.status(400).json({ msg: 'User not found' });
+        if (!user) return res.status(400).json({ message: 'User not found' });
 
         const isMatch = await user.comparePassword(password);
-        if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token });
+        const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ user: {id: user._id, username: user.username, email: user.email}, token });
     } catch (error) {
-        res.status(400).json({ msg: 'Error logging in', error: error.message });
+        res.status(400).json({ message: 'Error logging in', error: error.message });
     }
 };
